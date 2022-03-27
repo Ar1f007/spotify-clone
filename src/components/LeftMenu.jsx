bfc924af0dd75e8163b25d6fa585473ac5668eab
Ar1f007 <muhammadarif9416@yahoo.com>
2022-03-28 00:23:17 +0600

    left menu top part styles done

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -1,10 +1,24 @@
 import '../Styles/LeftMenu.css';
-
+import { FaSpotify, FaEllipsisH } from 'react-icons/fa';
+import { BiSearchAlt } from 'react-icons/bi';
 const LeftMenu = () => {
   return (
     <div className="leftMenu">
-      <div className="logoContainer"></div>
-      <div className="searchBox"></div>
+      <div className="logoContainer">
+        <i>
+          <FaSpotify />
+        </i>
+        <h2>Spotify</h2>
+        <i>
+          <FaEllipsisH />
+        </i>
+      </div>
+      <div className="searchBox">
+        <input type="text" placeholder="Search..." />
+        <i className="searchIcon">
+          <BiSearchAlt />
+        </i>
+      </div>
     </div>
   );
 };
