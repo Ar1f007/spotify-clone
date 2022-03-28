648faf8dd74cd03d4c7da8ea103614ea45bda91a
Ar1f007 <muhammadarif9416@yahoo.com>
2022-03-28 22:17:35 +0600

    menuList on main container styles done

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,10 +1,38 @@
+import { NavLink } from 'react-router-dom';
 import '../Styles/MainContainer.css';
 import { Banner } from './';
+import { FaUsers } from 'react-icons/fa';
 
 const MainContainer = () => {
   return (
     <div className="mainContainer">
       <Banner />
+
+      <div className="menuList">
+        <ul>
+          <li>
+            <NavLink to="/">Popular</NavLink>
+          </li>
+          <li>
+            <NavLink to="/albums">Albums</NavLink>
+          </li>
+          <li>
+            <NavLink to="/songs">Songs</NavLink>
+          </li>
+          <li>
+            <NavLink to="/fans">Fans</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </ul>
+        <p>
+          <i>
+            <FaUsers />
+          </i>
+          12.3M <span>Followers</span>
+        </p>
+      </div>
     </div>
   );
 };
