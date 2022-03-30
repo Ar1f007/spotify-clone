a560484e026d4642f37ac2a6f80895c7b86be56e
Ar1f007 <muhammadarif9416@yahoo.com>
2022-03-30 12:33:33 +0600

    right menu done

--- src/components/RightMenu.jsx
+++ src/components/RightMenu.jsx
@@ -1,6 +1,41 @@
 import '../Styles/RightMenu.css';
 
-const RightMenu = () => {
-  return <div className="rightMenu">RightMenu</div>;
-};
+import { FaCrown, FaBell, FaRegHeart, FaSun, FaCogs } from 'react-icons/fa';
+import Profile from '../img/profile.jpg';
+
+function RightMenu() {
+  return (
+    <div className="rightContainer">
+      <div className="goPro">
+        <i>
+          <FaCrown />
+          <p>
+            <span> Go</span>Pro
+          </p>
+        </i>
+
+        <i>
+          <FaBell />
+        </i>
+
+        <i>
+          <FaRegHeart />
+        </i>
+      </div>
+      <div className="profile">
+        <i>
+          <FaSun />
+        </i>
+        <i>
+          <FaCogs />
+        </i>
+
+        <div className="profileImage">
+          <img src={Profile} alt="profile" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export { RightMenu };
